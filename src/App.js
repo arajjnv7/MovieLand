49e3d36ffe532e8b8831a6ad0d5d129606e8e1fa
Aryan Raj <arajjnv@gmail.com>
2023-07-15 10:01:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ const App = () => {
                 movies?.length > 0 
                 ? (
                 <div className='container'>
-                {movies.map((movie) => (
-                    <MovieCard movie={movie}/>
+                {movies.map(movie => (
+                    <MovieCard movie={movie}/>  
                 ))}
             </div> ) : (
                 <div className='empty'>
